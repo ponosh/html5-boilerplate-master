074142e848228516f020bfae5f45dc17190731b1
ponosh <mrinalikamath@Mrinalis-MacBook-Pro.local>
2017-05-18 10:42:32 +0200

    Nav bar fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
-console.log('Hello world!');
-
-$(document).ready(function() {
-	console.log('ready');
-
-$(".menu-toggle").on('click', function() {
-  $(this).toggleClass("on");
-  $('.menu-section').toggleClass("on");
-  $("nav ul").toggleClass('hidden');
+$(".menu-toggle").click(function() {
+	console.log('click')
+	$(this).toggleClass('active');
+	$('ul[role="navigation"]').toggleClass('hidden');
+	var isActive = $(this).hasClass('active');
+	if (isActive) {
+		$('body').css({
+			'overflow': 'hidden'
+		});
+	} else {
+		$('body').removeAttr('style');
+	}
 });
